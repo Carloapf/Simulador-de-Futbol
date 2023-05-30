ab85e9fa4b99b28b81442483a8e162e239ae8d25
Carlo <carlo.pano@uabc.edu.mx>
2023-05-29 23:26:40 -0700

    Tabla ya pinta las posiciones e Histograma

--- interfaz-prototipo.py
+++ interfaz-prototipo.py
@@ -2,6 +2,15 @@ import tkinter as tk
 from tkinter import ttk
 import random
 import matplotlib.pyplot as plt
+import sys
+
+class StdoutRedirector:
+    def __init__(self, text_widget):
+        self.text_widget = text_widget
+
+    def write(self, message):
+        self.text_widget.insert("end", message)
+        self.text_widget.see("end")
 
 equipos = ["Real Madrid", "Barcelona", "Atlético de Madrid", "Sevilla", "Real Sociedad", "Villarreal", "Betis", "Athletic Bilbao", "Granada", "Valencia", "Levante", "Celta de Vigo", "Osasuna", "Getafe", "Cádiz", "Alavés", "Eibar", "Valladolid", "Elche", "Huesca"]
 
@@ -14,9 +23,9 @@ goleadores = {}
 
 # Definir jugadores por equipo
 jugadores_por_equipo = {
-    "Real Madrid": ["Cristiano Ronaldo", "Karim Benzema", "Gareth Bale"],
-    "Barcelona": ["Lionel Messi", "Antoine Griezmann", "Philippe Coutinho"],
-    "Atlético de Madrid": ["Luis Suárez", "João Félix", "Ángel Correa"],
+    "Real Madrid": ["Vinicius", "Karim Benzema", "Rodrygo"],
+    "Barcelona": ["Robert Lewandowski", "Dembele", "Raphinha"],
+    "Atlético de Madrid": ["Memphis", "João Félix", "Ángel Correa"],
     "Sevilla":["Tecatito","Youssef En-Nesyri","Suso"],
     "Real Sociedad":["Kubo","Mikel Oyarzabal","Carlos Fernadez"],
     "Villarreal": ["Gerard Moreno","Manu Trigeros","Yeremi Pino"],
@@ -28,7 +37,7 @@ jugadores_por_equipo = {
     "Celta de Vigo": ["Iago Aspas", "Carles Perez", "Goncalo Paciencia"], 
     "Osasuna": ["Abde Ezzalzouli", "Ruben Garcia", "Chimy Avila"], 
     "Getafe": ["Portu", "Borja Mayoral", "Jaime Mata"], 
-    "Cádiz": ["Alvaro Negredo", "Ruben Sobrino", "Anthony Lozano"], 
+    "Cádiz": ["Alvaro Negredo", "Ruben Sobrino", "Anthony Lozano"],
     "Alavés": ["Miguel de la Fuente", "Luis Roja", "Jason"], 
     "Eibar": ["Jon Bautista", "José Corpas", "Gustavo Blanco Leschuk"], 
     "Valladolid": ["Gonzalo Plata", "Kenedy", "Sergio Leon"], 
@@ -52,10 +61,32 @@ def iniciar_simulacion():
     equipos_habilidades = dict(zip(equipos, habilidades))
 
     def simular_partido(local, visitante):
-        habilidad_local = equipos_habilidades[local]
-        habilidad_visitante = equipos_habilidades[visitante]
-        resultado_local = random.randint(0, 5)
-        resultado_visitante = random.randint(0, 5)
+        #habilidad_local = equipos_habilidades[local]
+        #habilidad_visitante = equipos_habilidades[visitante]
+
+        habilidad_local = random.randint(0, equipos_habilidades[local])
+        habilidad_visitante = random.randint(0, equipos_habilidades[visitante])
+        diff = 0
+
+        if (habilidad_local + 8) > habilidad_visitante:
+            resultado_local = random.randint(1, 5)
+            diff = random.randint(1,3)
+            resultado_visitante = resultado_local - diff
+            if resultado_visitante < 0:
+                resultado_visitante = max(resultado_visitante, 0)
+
+        elif (habilidad_visitante + 8) > habilidad_local:
+            resultado_visitante = random.randint(1, 5)
+            diff = random.randint(1,3)
+            resultado_local = resultado_visitante - diff
+            if resultado_local < 0:
+                 resultado_local = max(resultado_local, 0)
+        elif (habilidad_local):
+            diff = random.randint(0,3)
+            resultado_local, resultado_visitante = diff
+
+        #resultado_local = random.randint(0, 5)
+        #resultado_visitante = random.randint(0, 5)
         #print(resultado_local,"y",resultado_visitante)
         if resultado_local > resultado_visitante:
             return local, resultado_local, resultado_visitante
@@ -93,6 +124,8 @@ def iniciar_simulacion():
        
             print(local, goles_local, goles_visitante, visitante)
         print()
+        equipos = [partido[0] if partido[2] == partido[0] else partido[1] if partido[2] == partido[1] else partido[0] for partido in partidos]
+
     
     
     # Código de simulación de temporada aquí
@@ -102,17 +135,33 @@ def iniciar_simulacion():
         equipos = equipos[1:] + [equipos[0]]
         simular_jornada(jornada, equipos, puntos, goleadores)
     
-    # Obtener los puntos de los equipos en una lista
-    #puntos_equipos = [puntos[equipo] for equipo in equipos]
-    puntos_equipos = list(puntos.values())
+    # Determinar el equipo campeón
+    campeon = max(puntos, key=puntos.get)
 
+    #puntos_equipos = list(puntos.values())
+    puntos_equipos = [puntos[equipo] for equipo in equipos]
+    puntos_equipos = sorted(puntos_equipos, reverse=True)
+    nombres_equipos = [equipo for equipo, _ in sorted(puntos.items(), key=lambda x: x[1], reverse=True)]
+
+    # Obtener el índice de la fila del campeón
+    indice_campeon = nombres_equipos.index(campeon)
 
     # Actualizar la tabla de posiciones
     tabla_posiciones.delete(*tabla_posiciones.get_children())
     clasificacion = sorted(puntos.items(), key=lambda x: x[1], reverse=True)
     for i, (equipo, puntos) in enumerate(clasificacion):
-        tabla_posiciones.insert('', 'end', iid=i, values=(i+1, equipo, puntos))
+        etiqueta = (i+1, equipo, puntos)
+        tags = ('campeon', 'top4', 'normal') if i == 0 else ('top4', 'normal') if 1 <= i <= 3 else ('normal',)
+        #tags = ('campeon', 'top4', 'normal') if i < 4 else ('normal',)
+        tabla_posiciones.insert('', 'end', iid=i, values=etiqueta, tags=tags)
+        
+        # Resaltar la fila del campeón
+        tabla_posiciones.tag_configure('campeon', background='yellow')
+        tabla_posiciones.tag_configure('top4', background='cyan')
+        tabla_posiciones.tag_configure('normal', background='white')
 
+    
+    
     # Actualizar la tabla de goleadores
     tabla_goleadores.delete(*tabla_goleadores.get_children())
     goleadores_ordenados = []
@@ -122,11 +171,14 @@ def iniciar_simulacion():
     goleadores_ordenados = sorted(goleadores_ordenados, key=lambda x: x[2], reverse=True)
     for i, (equipo, jugador, goles) in enumerate(goleadores_ordenados):
         tabla_goleadores.insert('', 'end', iid=i, values=(equipo, jugador, goles))
+    # Obtener los puntos de los equipos en una lista
     
+    
+
     # Crear el histograma de los puntos
     plt.figure(figsize=(10, 6))
     plt.bar(range(len(puntos_equipos)), puntos_equipos, align='center', edgecolor='black')
-    plt.xticks(range(len(equipos)), equipos, rotation=90)
+    plt.xticks(range(len(puntos_equipos)), nombres_equipos, rotation=90)
     plt.xlabel('Equipos')
     plt.ylabel('Puntos')
     plt.title('Distribución de puntos de los equipos')
@@ -137,7 +189,18 @@ def iniciar_simulacion():
    
 
 
-    
+# Crear una ventana secundaria para mostrar la salida de la consola
+ventana_secundaria = tk.Toplevel()
+ventana_secundaria.title("Consola")
+ventana_secundaria.geometry("800x600")
+
+# Crear un widget de texto para mostrar la salida de la consola
+texto_consola = tk.Text(ventana_secundaria)
+texto_consola.pack()
+
+# Redirigir la salida de la consola al widget de texto
+sys.stdout = StdoutRedirector(texto_consola)
+
 
 # Crear la ventana principal
 ventana = tk.Tk()
@@ -174,3 +237,6 @@ tabla_goleadores.pack(pady=20)
 
 # Ejecutar la ventana principal
 ventana.mainloop()
+
+# Mostrar la ventana secundaria
+ventana_secundaria.mainloop()
